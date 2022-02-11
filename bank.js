245527c6cf36c516bf16e4d5d778c033ef5ac2e3
Toriqul-Leon <yeitoriqul@gmail.com>
2022-02-12 02:11:46 +0600

    Fried

--- bank.js
+++ bank.js
@@ -7,34 +7,47 @@ function getInputValue(inputId) {
   return amountValue;
 }
 
+//   !Get and update current deposit
 function updateTotalField(totalFieldId, newDepositAmount) {
-  //   !Get and update current deposit
   const totalElement = document.getElementById(totalFieldId);
   const totalText = totalElement.innerText;
   const previousTotal = parseFloat(totalText);
   totalElement.innerText = newDepositAmount + previousTotal;
 }
-
-function updateBalance(amount) {
-  //   !Add Balance
+// !Get current balance
+function getCurrentBalance() {
   const balanceAmount = document.getElementById("balance-total");
   const balanceAmountText = balanceAmount.innerText;
   const previousBalanceAmount = parseFloat(balanceAmountText);
+  return previousBalanceAmount;
+}
+//   !Add Balance
+function updateBalance(amount) {
+  const balanceAmount = document.getElementById("balance-total");
+  const previousBalanceAmount = getCurrentBalance(amount);
   balanceAmount.innerText = previousBalanceAmount + amount;
 }
 const depositButton = document.getElementById("deposit-button");
-
 depositButton.addEventListener("click", function () {
   const newDepositAmount = getInputValue("deposit-input");
   //   !Get and update current deposit
-  updateTotalField("deposit-total", newDepositAmount);
-  updateBalance(newDepositAmount);
+  if (newDepositAmount > 0) {
+    updateTotalField("deposit-total", newDepositAmount);
+    updateBalance(newDepositAmount);
+  } else {
+    alert("Enter Positive Amount");
+  }
 });
 
 // !Handle Withdraw
 const withdrawButton = document.getElementById("withdraw-button");
 withdrawButton.addEventListener("click", function () {
   const newWithdrawInput = getInputValue("withdraw-input");
-  updateTotalField("withdraw-total", newWithdrawInput);
-  updateBalance(-newWithdrawInput);
+  const rightNow = getCurrentBalance();
+  if (newWithdrawInput > 0 && newWithdrawInput < rightNow) {
+    updateTotalField("withdraw-total", newWithdrawInput);
+    updateBalance(-newWithdrawInput);
+  } else {
+    alert("Balance Exceeded");
+  }
 });
